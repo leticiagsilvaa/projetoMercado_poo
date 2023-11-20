d6e9bbb3b43932cd04b6a6862b8f65acd15533f8
RuanRodrigues1 <142950355+RuanRodrigues1@users.noreply.github.com>
2023-11-20 15:24:08 -0300

    Update Pagamento.java

--- Mercadinho/src/classes/Pagamento.java
+++ Mercadinho/src/classes/Pagamento.java
@@ -1,25 +1,66 @@
-package classes;
-
 import java.time.LocalDate;
 
-    public class Pagamento{
-    //atributos
+public class Pagamento {
+    // atributos
     protected String codigo;
     private double valor;
     private LocalDate data;
     private String status;
     private int idpagamento;
 
-public Pagamento(String codigo , double valor, LocalDate data, String status, int idPagamento) {
+    public Pagamento(String codigo, double valor, LocalDate data, String status, int idPagamento) {
         this.codigo = codigo;
         this.valor = valor;
         this.data = data;
         this.status = status;
         this.idpagamento = idPagamento;
     }
-//metodos
-public void escolherPagamento(){
-    
-}
 
+    // getters
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getIdPagamento() {
+        return idpagamento;
+    }
+
+    // setters
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setIdPagamento(int idPagamento) {
+        this.idpagamento = idPagamento;
+    }
+
+    // métodos
+    public void escolherPagamento() {
+        // implementação do método escolherPagamento
+    }
 }
