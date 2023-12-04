4c68bc0c82bb173caa6da670ced70e12c9a53369
Malu Vasconcelos <96549606+maluvasc@users.noreply.github.com>
2023-12-04 12:37:59 -0300

    Update Pessoa.java

--- Mercadinho/src/classes/Pessoa.java
+++ Mercadinho/src/classes/Pessoa.java
@@ -3,27 +3,15 @@ package classes;
 public abstract class Pessoa {
     private String cpf;
     private String nome;
-    private String endereco;
-    private String telefone;
 
-    public Pessoa(String cpf, String nome, String endereco, String telefone) {
+    public Pessoa(String cpf, String nome) {
         this.cpf = cpf;
         this.nome = nome;
-        this.endereco = endereco;
-        this.telefone = telefone;
     }
 
     // getters
     public String getNome() {
         return nome;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public String getTelefone() {
-        return telefone;
     }
 
     public String getCpf() {
@@ -35,17 +23,10 @@ public abstract class Pessoa {
         this.nome = nome;
     }
 
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
+    
     // Métodos abstratos
 
     public abstract void exibirInformacoes();
@@ -56,9 +37,6 @@ public abstract class Pessoa {
 
     public String toString() {
         return getNome() + "{" +
-                "cpf: " + getCpf() + '\'' +
-                ", endereco: '" + getEndereco() + '\'' +
-                ", telefone: '" + getTelefone() + '\'' +
-                '}';
+                "cpf: " + getCpf()+ "}";
     }
 }
