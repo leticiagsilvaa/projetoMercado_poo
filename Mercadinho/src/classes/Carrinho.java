3431570ce0083ed964c2f1fa4b488ac4fffb7ef2
Malu Vasconcelos <96549606+maluvasc@users.noreply.github.com>
2023-12-04 12:31:00 -0300

    Update Carrinho.java

--- Mercadinho/src/classes/Carrinho.java
+++ Mercadinho/src/classes/Carrinho.java
@@ -1,46 +1,71 @@
 package classes;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Carrinho {
     private Cliente cliente;
-    private List<ItemDoCarrinho> produto;
-    private Carrinho carrinho;
+    private List<ItemDoCarrinho> produtos  = new ArrayList<>();
+    private int idCarrinho;
     private double subTotal;
 
-    public Carrinho(Cliente cliente, List<ItemDoCarrinho> produto, int idCarrinho, double subTotal) {
-        this.cliente = cliente;
-        this.produto = produto;
+    public Carrinho(int idCarrinho){
         this.idCarrinho = idCarrinho;
-        this.subTotal = subTotal;
+        this.subTotal = 0.0;
+    }
+    
+    // Métodos de carrinho
+
+    public void adicionarAoCarrinho(ItemDoCarrinho item) {
+        produtos.add(item);
+        subTotal += item.getValor() * item.getQuantidade();
     }
 
-    // Metodos de carrinho
-    // implementar o esvaziar carrinho
+    public void removerDoCarrinho(ItemDoCarrinho item) {
+        produtos.remove(item);
+        subTotal -= item.getValor();
+    }
 
-    // Metodos Getters e Setters da classe carrinho
+    public void esvaziarCarrinho() {
+        produtos.clear();
+        subTotal = 0.0;
+    }
+
+    // Métodos Getters e Setters da classe Carrinho
     public int getIdCarrinho() {
         return idCarrinho;
     }
+
     public double getSubTotal() {
         return subTotal;
-    }  
+    }
+
     public Cliente getCliente() {
         return cliente;
     }
-    public List<ItemDoCarrinho> getItemDoCarrinho() {
-        return produto;
+
+    public List<ItemDoCarrinho> getProdutosNoCarrinho() {
+        return produtos;
     }
 
     public void setIdCarrinho(int idCarrinho) {
         this.idCarrinho = idCarrinho;
     }
-        public void setSubTotal(double subTotal) {
+
+    public void setSubTotal(double subTotal) {
         this.subTotal = subTotal;
     }
+
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
     }
-    public void setItem(List<ItemDoCarrinho> produto) {
-        this.produto = produto;
+
+    public void setProdutos(List<ItemDoCarrinho> produtos) {
+        this.produtos = produtos;
+        recalcularSubTotal();
+    }
+
+    private void recalcularSubTotal() {
+        subTotal = produtos.stream().mapToDouble(ItemDoCarrinho::getValor).sum();
     }
 }
