1d49525f57d45e23f7fba0f96efab507898396a4
Malu Vasconcelos <96549606+maluvasc@users.noreply.github.com>
2023-12-04 12:34:35 -0300

    Update PagamentoCartao.java

--- Mercadinho/src/classes/PagamentoCartao.java
+++ Mercadinho/src/classes/PagamentoCartao.java
@@ -1,42 +1,37 @@
 package classes;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class PagamentoCartao extends Pagamento {
-    private String numeroCartao;
-    private String tipoCredito;
-    private String tipoDebito;
-
+    
     // Construtor
-    public PagamentoCartao(double valor, LocalDate data, String descricao, String status, int idPagamento,
-            String numeroCartao, String tipoCredito, String tipoDebito) {
-        super(valor, data, descricao, status, idPagamento);
-        this.numeroCartao = numeroCartao;
-        this.tipoCredito = tipoCredito;
-        this.tipoDebito = tipoDebito;
+    public PagamentoCartao(String codigo, double valor, LocalDate data, String status, Venda venda) {
+        super(codigo, valor, data, status, venda);
     }
 
-    // Método para parcelar crédito
-    public void parcelarCredito(int numeroParcelas) {
-        if ("CREDITO".equals(this.tipoCredito.toUpperCase())) {
-            double valorParcela = this.getValor() / numeroParcelas;
-            System.out.println("Compra parcelada em " + numeroParcelas + " vezes de R$ " + valorParcela
-                    + " no cartão de crédito.");
-            // Aqui você pode adicionar lógica adicional, como salvar as informações do
-            // parcelamento.
-        } else {
-            System.out.println("Este cartão não suporta pagamento parcelado no crédito.");
-        }
-    }
+    public void pagar() {
+        System.out.println("Deseja pagar no crédito ou débito?");
+        System.out.println("1- Crédito");
+        System.out.println("2 - Débito");
+        Scanner input = new Scanner(System.in);
+        String tipoPagamento = input.nextLine();
+        input.close();
+        if ("1".equals(tipoPagamento)){
+        pagarCredito();
+    };
+        if ("2".equals(tipoPagamento)){
+        pagarDebito();
+    };
+ }
 
-    // Método para realizar o pagamento no cartão de débito
+    public void pagarCredito(){
+        double valorParcela = venda.getValorTotal();
+        System.out.println("Compra de " + valorParcela + " aprovada no cartão de crédito.");
+        } 
+
     public void pagarDebito() {
-        if ("DEBITO".equals(this.tipoDebito.toUpperCase())) {
-            System.out.println("Compra no valor de R$ " + this.getValor() + " debitada no cartão de débito.");
-            // Aqui você pode adicionar lógica adicional, como atualizar o status do
-            // pagamento.
-        } else {
-            System.out.println("Este cartão não suporta pagamento no débito.");
+        double valorParcela = venda.getValorTotal();
+        System.out.println("Compra no valor de R$ " + valorParcela + " debitada no cartão de débito.");
         }
-    }
 }
